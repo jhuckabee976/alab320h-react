2398877f267b7eeae491a6bdf3f3e5ecf8c169fb
jhuckabee976 <jhuckabee976@gmail.com>
2024-09-10 09:25:17 -0600

    second commit

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -3,7 +3,7 @@ function Article() {
         <article>
       <h3>11/12/20</h3>
       <h2>On the Street in Brooklyn</h2>
-            <img src="./images\blog-image-1.jpg"
+            <img src="./blog-image-1.jpg"
                 alt="Single lady in orange dress on empty sidewalk in Brooklyn" />
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, recusandae! Provident, fuga quasi dolor non hic maiores, consectetur amet sed expedita atque eius voluptatibus, quo facere? Molestias velit corrupti eveniet.</p>
 
--- src/components/Article2.jsx
+++ src/components/Article2.jsx
@@ -3,7 +3,7 @@ function Article2() {
         <article>
       <h3>11/11/20</h3>
       <h2>Vintage in Vogue</h2>
-      <img src="./images\blog-image-2.jpg" alt="Vintage photo of three people in Vogue" />
+      <img src="./blog-image-2.jpg" alt="Vintage photo of three people in Vogue" />
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum natus eum ipsam veniam voluptate dolor libero at sit nisi repellat aut, a porro. Reprehenderit, impedit sapiente. Voluptatibus similique velit nostrum.</p>
 
       <a href="#"className="continue-btn">Continues...</a>
